fce839a1ff5476a58770cd7017e536f57ec155fe
houtsandrew <houtsandrew@gmail.com>
2016-02-25 12:56:57 -0800

    Revert "Revert "further development of class diagram""
    This reverts commit 5246edfdfa3c1431de633ae8492eed204aec26f8.

---
+++ CIS22B_FinalProject_Workflow/testWorkflow/Cashier.vb
@@ -0,0 +1,19 @@
+﻿Public Class Cashier
+    ''' <summary>
+    ''' takes in the BookDatabase by reference, NOT VALUE
+    ''' </summary>
+    Private Property database As testWorkflow.BookDatabase
+        Get
+            Return Nothing
+        End Get
+        Set(value As testWorkflow.BookDatabase)
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' displays the cashier main menu and asks the user for input
+    ''' </summary>
+    Public Sub mainMenu()
+
+    End Sub
+End Class
