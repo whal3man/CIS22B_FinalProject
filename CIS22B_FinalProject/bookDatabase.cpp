1c99e698d0ccc18343983f6149a15ce3a863836c
whal3man <lucke97@gmail.com>
2016-03-01 23:22:43 -0800

    made a book Database class and started on the header file

---
+++ CIS22B_FinalProject/bookDatabase.cpp
@@ -0,0 +1,30 @@
+using namespace std;
+#include <iostream>
+using namespace std;
+#include <iostream>
+#include "Header.h"
+
+void addBook()
+{
+
+}
+int bookDatabase::getPrice(int identifier)
+{
+
+}
+void bookDatabase::printBook(int identifier)
+{
+
+}
+void bookDatabase::printISBN(int isbn)
+{
+
+}
+void bookDatabase::removeBook(int isbn)
+{
+
+}
+int bookDatabase::searchIndentifier(int identifier)
+{
+
+}
